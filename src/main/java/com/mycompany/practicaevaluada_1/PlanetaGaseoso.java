210ca804d3315b7d8466a6469044cbcaba3ac9cc
amayH2000fide <amay90782@ufide.ac.cr>
2025-10-03 13:23:46 -0600

    inicio de clases

---
+++ src/main/java/com/mycompany/practicaevaluada_1/PlanetaGaseoso.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practicaevaluada_1;
+
+/**
+ *
+ * @author zid
+ */
+public class PlanetaGaseoso {
+    
+}
